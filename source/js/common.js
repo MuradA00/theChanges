553910eef5d84cc79afd6a9954c763beceea476a
Murad <abishlimurad@gmail.com>
2022-10-20 04:39:41 +0300

    i love javascript

--- source/js/common.js
+++ source/js/common.js
@@ -1,11 +1,60 @@
 const windowHeight = window.innerHeight,
       heroSection = document.querySelector('.hero'),
-      header = document.querySelector('.header');
+      header = document.querySelector('.header'),
+      saturnImage = document.querySelector('.hero__jupiter'),
+      guideSection = document.querySelector('.guide'),
+      marsPlanet = document.querySelector('.hero__mars'),
+      earthPlanet = document.querySelector('.hero__earth')
+      planetRowImages = document.querySelectorAll('.planet-row__image'),
+      planetSection = document.querySelector('.planet');
+      let scrollPosition = window.pageYOffset;
+
+let planetSectionMargin = getComputedStyle(planetSection).marginTop;
+
+const scrollOptions = {
+  rootMargin: `-${planetSectionMargin} 0px 0px 0px`,
+  threshold: .4
+}
+
+const scrollObserver = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    planetRowImages.forEach(planet => {
+      if (entry.isIntersecting) {
+        planet.classList.add('_activeImage')
+        marsPlanet.classList.add('vanishedPlanet')
+        earthPlanet.classList.add('vanishedPlanet')
+      } else if (!entry.isIntersecting) {
+        planet.classList.remove('_activeImage')
+      }
+    })
+
+  })
+}, scrollOptions)
+
+scrollObserver.observe(planetSection);
+
+
+
+function changeOnScroll() {
+  let scrollPosition = window.pageYOffset;
+  if (scrollPosition > 0) {
+    marsPlanet.style.transform = `translateY(${scrollPosition}px)`
+    earthPlanet.style.transform = `translateY(${scrollPosition * 1.7}px)`
+  }
+}
+
+
 
 function setSpacing() {
   const headersHeight = header.clientHeight;
   heroSection.style.paddingTop = headersHeight + 'px';
 }
+
+window.addEventListener('scroll', () => {
+  changeOnScroll();
+  console.log(planetSectionMargin)
+});
+
 
 setSpacing();
 
